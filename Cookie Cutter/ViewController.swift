4e9740f9e16fdce88fb11eaf771bc227fa0e3ffe
Chris Lowe <chrislowe@chriss-imac.gateway.pace.com>
2014-07-01 14:47:43 -0500

    Initial Commit

---
+++ Cookie Cutter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cookie Cutter
+//
+//  Created by Chris Lowe on 7/1/14.
+//  Copyright (c) 2014 Chris Lowe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
